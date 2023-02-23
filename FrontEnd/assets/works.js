aec7ea0a331712331a78c521e603d7c89505bc68
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-02-23 09:53:37 +0100

    Récupération des travaux depuis le back-end

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -3,5 +3,22 @@ fetch("http://localhost:5678/api/works")
   .then(response => response.json())
   .then(data => {
     console.log("Liste des projets : ", data);
+    for (let i = 0; i < data.length; i++) {
+      const gallery = document.querySelector(".gallery"); 
+
+      let figure = document.createElement("figure");
+      let img = document.createElement("img");
+      let figcaption = document.createElement("figcaption");
+
+      img.src = data[i].imageUrl;
+      img.alt = data[i].title;
+
+      figcaption.textContent = data[i].title;
+
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+
+      gallery.appendChild(figure);
+    }
   })
   .catch(error => console.error(error));
