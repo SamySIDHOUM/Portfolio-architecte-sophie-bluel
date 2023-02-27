2ad348095372747f7b3e1538a7e877e2a17b8af3
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-02-27 14:20:37 +0100

    Ajout des filtres pour afficher les travaux par catégorie

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -22,3 +22,45 @@ fetch("http://localhost:5678/api/works")
     }
   })
   .catch(error => console.error(error));
+
+
+// Ajout des filtres pour afficher les travaux par catégorie
+fetch("http://localhost:5678/api/categories")
+  .then(response => response.json())
+  .then(categories => {
+    const gallery = document.querySelector(".gallery"); 
+    const filters = document.createElement("div");
+    const buttonAll = document.createElement("button");
+
+    filters.classList.add("filter-buttons");
+    gallery.before(filters);
+    buttonAll.innerText = "Tous";
+    filters.appendChild(buttonAll);
+    filters.className = "active";
+
+    buttonAll.addEventListener("click", function(){
+      document.querySelector(".gallery").innerHTML = "";
+      data.forEach(function(categories) {
+        createCategoriesElement(categories);
+      });
+    });
+
+    for(let i=0; i < categories.length; i++){
+      const filterButton = document.createElement("button");
+      filterButton.innerText = categories[i].name;
+      filters.appendChild(filterButton);
+
+      filterButton.addEventListener("click", function(){
+        const filteredGallery= gallery.filter(function(categories){
+          return categories.category.name === categories[i].name;
+        });
+        document.querySelector(".gallery").innerHTML = "";
+        filteredData.forEach(function(categories) {
+          createCategoriesElement(categories);
+        });
+      });
+    };
+
+  })
+  .catch(error => console.error(error));
+
