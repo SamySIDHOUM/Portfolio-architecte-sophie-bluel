115841a78dada41ed202ad6462aead0a3b82a63b
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-03-10 10:36:36 +0100

    Amélioré le message d’erreur si user ou password incorrect

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -3,10 +3,6 @@ const form = document.querySelector("#login");
 //const tokenKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4";
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
-/*const user = {
-    email: email, 
-    password: password
-};*/
 // Écoute de l'événement de soumission du formulaire
 form.addEventListener("submit", (event) => {
   // Empêcher l'envoi du formulaire par défaut
@@ -15,7 +11,6 @@ form.addEventListener("submit", (event) => {
   // Récupération des valeurs des champs
   const email = emailInput.value;
   const password = passwordInput.value;
-  console.log("Valeurs des champs :", email, password);
 
   // Envoi de la requête POST à l'API pour se connecter avec les informations de connexion saisies
   fetch("http://localhost:5678/api/users/login", {
@@ -30,15 +25,23 @@ form.addEventListener("submit", (event) => {
   })
     .then((response) => response.json()) // Transformation de la réponse en objet JSON
     .then((data) => {
-        console.log("Données renvoyées par l'API :", data);
+        const login = document.getElementById("login");
       // Vérification des données renvoyées par l'API
       if (data.message === "user not found" || data.error) {
         // Affichage d'un message d'erreur si l'utilisateur n'a pas été trouvé ou si le mot de passe est incorrect
-        alert("Erreur dans l'identifiant ou le mot de passe");
+        let messageError = document.createElement("span");
+        messageError.innerText = "Erreur dans l'identifiant ou le mot de passe";
+        messageError.style.color = "red";
+        messageError.id ="messageError";
+
+        if (!document.getElementById("messageError")){
+            login.appendChild(messageError); // Ajout du message d'erreur à la page de connexion
+        }
       } else {
         // Redirection vers la page d'accueil si l'utilisateur est connecté
         window.location.href = "./index.html";
-        console.log("Connexion réussie !");
+        const messageError =document.getElementById("messageError");
+        login.removeChild(messageError);// Suppression du message d'erreur de la page de connexion s'il existe
       }
     })
     .catch((error) => console.error(error));
