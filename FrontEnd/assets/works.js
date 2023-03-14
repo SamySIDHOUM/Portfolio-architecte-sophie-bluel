7a650bbb6a808700f94ec0ac8ddbcc3dc90ca7c6
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-03-14 09:53:30 +0100

    ajout modal

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -79,5 +79,47 @@ fetch("http://localhost:5678/api/categories")
       }
     }
   }
+
+
+//Recuperation de token
+const token = localStorage.getItem("token");
+
+// Ajouter un bouton "Modifier" 
+/*for (let i = 0; i < data.length; i++) {
+  
+  let button = document.createElement("button");
+  button.innerHTML = '<i class="fa fa-pencil"></i>';
+  button.addEventListener("click", function() {
+    showModal(data[i]);
+  });
+  figure.appendChild(button); 
+}*/
+// Créer une fenêtre modale
+const modal = document.createElement("div");
+modal.classList.add("modal");
+
+// Créer le contenu de la fenêtre modale
+const modalContent = document.createElement("div");
+modalContent.classList.add("modal-content");
+
+// Ajouter le titre à la fenêtre modale
+const modalTitle = document.createElement("h2");
+modalTitle.textContent = "Galerie photo";
+modalContent.appendChild(modalTitle);
+
+// Ajouter le contenu à la fenêtre modale
+const modalText = document.createElement("p");
+modalText.textContent = "";
+modalContent.appendChild(modalText);
+
+// Ajouter le contenu à la fenêtre modale
+modal.appendChild(modalContent);
+console.log(modalContent);
+
+// Ajouter la fenêtre modale au document
+document.body.appendChild(modal);
+
+
+
   
 
