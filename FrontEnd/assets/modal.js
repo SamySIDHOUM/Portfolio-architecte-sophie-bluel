90fc6d3d516bbfe8a53b459aab1b98125a31de59
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-03-28 09:59:31 +0200

    Création d'un conteneur et de bouton

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -9,13 +9,38 @@ modalContent.classList.add("modal-content");
 // Ajout d'un bouton pour fermer la fenêtre modale
 const closeButton = document.createElement("button");
 closeButton.textContent = "X";
-closeButton.classList.add("close-button");
+closeButton.classList.add("close-bts");
 modalContent.appendChild(closeButton);
 
 // Ajout du contenu dans la fenêtre modale
 const modalText = document.createElement("p");
 modalText.textContent = "Galerie photo";
 modalContent.appendChild(modalText);
+
+// Création d'un conteneur pour les images de la galerie
+const galleryContainer = document.createElement("div");
+galleryContainer.classList.add("gallery-container");
+modalContent.appendChild(galleryContainer);
+
+// Ajout des images de la galerie dans le conteneur
+const gallery = document.querySelector(".gallery"); 
+    ///TODO importer les image///
+
+// Création d'un bouton pour ajouter une photo à la galerie
+const addButton = document.createElement("button");
+addButton.textContent = "Ajouter une photo";
+addButton.classList.add("add-bts");
+    ///TODOle code pour ajouter///
+
+// Création d'un bouton pour supprimer la galerie
+const deleteGallery = document.createElement("a");
+deleteGallery.textContent = "Supprimer la galerie";
+deleteGallery.classList.add("delete-gal");
+    ///TODO le code pour supprimer///
+
+// Ajout des boutons à la fenêtre modale
+modalContent.appendChild(addButton);
+modalContent.appendChild(deleteGallery);
 
 modal.appendChild(modalContent);
 
