4e7c23814e53b1c1329ba6a79381166ed871351a
Samy SIDHOUM <samysidhoum6@gmail.com>
2023-04-13 20:54:51 +0200

    correctifs apportés au éléments à ajouter dans le DOM

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -27,14 +27,14 @@ fetch("http://localhost:5678/api/works")
 
       figcaption.textContent = "éditer";
       deleteIcon.classList.add("fa-solid", "fa-trash-can", "delete-icon");
+      deleteIcon.setAttribute("data-id",data[i].id);
 
       figcaption.appendChild(deleteIcon);
       figure.appendChild(img);
       figure.appendChild(figcaption);
       figure.classList.add("figure-modal-add");
-
+     
       gallery.appendChild(figure);
-      //modalContent.appendChild(gallery); 
     }
   
   })
@@ -117,12 +117,12 @@ fetch("http://localhost:5678/api/categories")
   })
   .catch(error => console.error(error));
 
-//Récupérer le formulaire et le bouton submit
+ // Récupérer le formulaire et le bouton submit
 const form = document.getElementById("form-addwork");
 const submitButton = document.getElementById("submit-work");
 const imgButton = document.getElementById("add-imgbutton");
 
-//Récupérer les éléments du formulaire
+// Récupérer les éléments du formulaire
 const titleInput = document.getElementById("title-img");
 
 //Ecouter le clic sur le bouton Ajouter photo
@@ -133,26 +133,20 @@ imgButton.addEventListener("change", function() {
     const imageUrl = reader.result;
     const imgPreview = document.createElement("img");
     imgPreview.src = imageUrl;
+    imgPreview.classList.add("img-preview");
+
+    // Masquer les autres éléments et afficher l'image
+    imgPreview.style.display = "block";
+    imgButton.style.display = "none";
+    document.getElementById("input-container").style.display = "none";
+    document.querySelector("#img-container p").style.display = "none";
     document.getElementById("img-container").appendChild(imgPreview);
   });
   reader.readAsDataURL(file);
 });
 
-//Ecouter le clic sur le bouton Valider
-submitButton.addEventListener("click", function(event) {
-  event.preventDefault(); // Empêcher le rechargement de la page
-  // Récupérer les valeurs du formulaire
-  const title = titleInput.value;
-  const category = categorySelect.value;
-  const imageUrl = document.querySelector("#img-container img").src;
-
-  // Vérifier si les champs sont remplis
-  if (title === "" || category === "" || imageUrl === "") {
-    alert("Veuillez remplir tous les champs");
-    return;
-  }
-
-  // Créer les éléments à ajouter dans le DOM
+// Fonction pour créer les éléments à ajouter dans le DOM
+function createGalleryItem(title, imageUrl) {
   const figure = document.createElement("figure");
   const img = document.createElement("img");
   const figcaption = document.createElement("figcaption");
@@ -170,23 +164,42 @@ submitButton.addEventListener("click", function(event) {
   figure.appendChild(figcaption);
   figure.classList.add("figure-modal-add");
   gallery.appendChild(figure);
+}
 
-  // Réinitialiser le formulaire
-  form.reset();
-  // Masquer la modal-addwork
-  modalAddwork.style.display = "none";
-  // Afficher la modal-content
-  modalContent.style.display = "block";
-  // Masquer la modal
-  modal.style.display = "none";
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault(); // Empêcher le rechargement de la page
+  // Récupérer les valeurs du formulaire
+  const title = titleInput.value;
+  const category = categorySelect.value;
+  const imageUrl = document.querySelector("#img-container img").src;
+
+  // Vérifier si les champs sont remplis
+  if (title === "" || category === "" || imageUrl === "") {
+    //alert("Veuillez remplir tous les champs");
+    let messageErrorInput = document.createElement("span");
+        messageErrorInput.innerText = "Veuillez remplir tous les champs";
+        messageErrorInput.style.color = "red";
+        messageErrorInput.id ="messageErrorInput";
+        console.log(messageErrorInput);
+    return;
+    
+  }
+  
+  // Ajouter la classe "active" au bouton "submitButton"
+  submitButton.classList.add("active");
 
   // Récupérer le token depuis la sessionStorage
   const token = sessionStorage.getItem("token");
+  console.log(token)
 
-  // Envoyer l'image à l'API
+  // Créer les données à envoyer à l'API
   const formData = new FormData();
   formData.append("image", imgButton.files[0]);
+  formData.append("title", title);
+  formData.append("category", categorySelect);
+  console.log(formData);
 
+  // Effectuer la requête pour envoyer l'image à l'API
   fetch("http://localhost:5678/api/works", {
     method: "POST",
     headers: {
@@ -202,12 +215,23 @@ submitButton.addEventListener("click", function(event) {
     return response.json();
   })
   .then(data => {
+    // Créer les éléments à ajouter dans le DOM
+    createGalleryItem(title, data.imageUrl);
+
+    // Réinitialiser le formulaire
+    form.reset();
+    // Masquer la modal-addwork
+    modalAddwork.style.display = "none";
+    // Afficher la modal-content
+    modalContent.style.display = "block";
+    // Masquer la modal
+    modal.style.display = "none";
+
     console.log(data);
   })
   .catch(error => {
     console.error(error);
   });
-
 });
 
 
